1fbcc15326f2fce9427c662699d3a37acc2e721b
Dylan Montandon <dylan.montandon@he-arc.ch>
2019-02-25 15:43:45 +0100

    correction

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,4 +1,5 @@
 
+import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
@@ -15,40 +16,42 @@ import static org.junit.Assert.fail;
  * @author baptiste.andres
  */
 public class StringCalculatorTest {
+
+    private StringCalculator sut;
+
+    //tu aurais pu utiliser l'annotation "before". Ca t'évite d'appeler le constructeur à chaque test.
+    @Before
+    public void setup() {
+        sut = new StringCalculator();
+    }
     
     @Test
         public void emptyStringShouldReturnZero() {
-        StringCalculator sut = new StringCalculator();
         assertEquals(0, sut.Add(""));
     }
         
     @Test
     public void oneNumberShouldReturnItseft() {
-        StringCalculator sut = new StringCalculator();
         assertEquals(3, sut.Add("3"));
     }
     
     @Test
     public void twoNumbersShouldBeAdded() {
-        StringCalculator sut = new StringCalculator();
         assertEquals(7, sut.Add("2,5"));
     }
     
     @Test
     public void supportNewLineAsSeparator() {
-        StringCalculator sut = new StringCalculator();
         assertEquals(6, sut.Add("1\n2,3"));
     }
     
     @Test
     public void supportForCustomDelimiter() {
-        StringCalculator sut = new StringCalculator();
         assertEquals(10, sut.Add(";\n1;2;3;4"));
     }
     
     @Test
     public void negativeNotSupported() {
-        StringCalculator sut = new StringCalculator();
         try {
             sut.Add("-1,4");
             fail("exception should have been thrown");
@@ -60,7 +63,6 @@ public class StringCalculatorTest {
     
     @Test
     public void NumbersGreaterThan1000NotIncludedInSum() {
-        StringCalculator sut = new StringCalculator();
         assertEquals(1009, sut.Add("3,1000,1001,6,1234"));
     }
     
